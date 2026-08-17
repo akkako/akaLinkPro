eea1f1f8bd4fba3479e1d3d6306ec9cd3a2d4a18
akako <69710421+akkako@users.noreply.github.com>
2026-08-17 21:04:11 +0800

    add jtag seq asm file

---
+++ firmware/application_5301/src/dap/JTAG_DP/JTAG_DP.h
@@ -0,0 +1,15 @@
+#ifndef JTAG_DP_H
+#define JTAG_DP_H
+
+#include <stdint.h>
+
+void JTAG_Sequence_Slow(uint32_t delay, uint32_t info, const uint8_t *tdi, uint8_t *tdo);
+uint32_t JTAG_ReadIDCode_Slow(void);
+void JTAG_WriteAbort_Slow(uint32_t data);
+void JTAG_IR_Slow(uint32_t ir);
+uint8_t JTAG_Transfer_Slow(uint32_t request, uint32_t *data);
+
+
+void JTAG_Sequence_GPIO_ASM_30M(uint32_t info, const uint8_t *tdi, uint8_t *tdo);
+
+#endif
